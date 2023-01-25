e673a4fb84f9971476a059d74de77363ec1f9fb0
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-25 00:03:39 -0500

    Setup rate limiting

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const express = require('express')
+const rateLimit = require('express-rate-limit');
 const initGetRouter = require('./routes/getRouter')
 const initPostRouter = require('./routes/postRouter')
 const app = express()
+
+const apiLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
@@ -11,6 +19,8 @@ app.use((req, res, next) => {
     res.locals.user = req.user
     next()
 })
+
+app.use('/api', apiLimiter)
 
 initGetRouter(app)
 initPostRouter(app)
