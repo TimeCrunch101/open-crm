c404a7b536f40451e2400d8bb01833ed4ac88923
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-24 23:06:09 -0500

    init

---
+++ server/controllers/authController.js
@@ -0,0 +1,82 @@
+const DATABASE = require('./databaseController')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { v4: uuidv4 } = require('uuid')
+
+exports.isAuthenticated = async (req, res, next) => {
+    const token = req.headers.authorization.split(' ',2)
+    try {
+        const user = jwt.verify(token[1],process.env.JWT_SKEY)
+        res.user = user
+
+        next()
+    } catch (error) {
+        res.status(401).json({
+            message: 'You are unauthorized'
+        })
+    }
+}
+
+exports.isNotAuthenticated = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ',2)
+        jwt.verify(token[1],process.env.JWT_SKEY)
+        res.status(400).json({
+            message: 'You are already logged in'
+        })
+    } catch (error) {
+        next()
+    }
+}
+
+exports.logIn = async(req, res) => {
+    const user = await DATABASE.getUserByEmail(req.body.username)
+    if (user === undefined) {
+        res.status(401).json({
+            message: 'Your username or password is incorrect',
+        })
+    } else {
+        const passMatch = await bcrypt.compare(req.body.password, user.password)
+        if (passMatch) {
+            const token = jwt.sign({
+                uuid: user.uuid,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                fullName: user.fullName,
+                email: user.email
+            },process.env.JWT_SKEY,{expiresIn: '30m'})
+
+            res.status(200).json({
+                message: `Welcome ${user.fullName}`,
+                token: token
+            })
+        } else {
+            res.status(401).json({
+                message: 'Your username or password is incorrect',
+            })
+        }
+    }
+}
+
+exports.register = async (req, res) => {
+    const {
+        firstName,
+        lastName,
+        email,
+        password
+    } = req.body
+    const hash = bcrypt.hashSync(password,10)
+    const fullName = `${firstName} ${lastName}`
+    const uuid = uuidv4()
+    try {
+        await DATABASE.createUser(uuid, firstName, lastName, fullName, email, hash)
+        res.status(200).json({
+            message: 'You have successfully registered'
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: error.message,
+            cause: error.cause
+        })
+    }
+}
--- server/controllers/getController.js
+++ server/controllers/getController.js
@@ -1,5 +1,17 @@
-import "./databaseController.js"
+const { DB } = require('../config/db')
+const DATABASE = require('./databaseController')
 
-export const rootPath = (req, res) => {
-    res.status(200).json({message: 'Got Req'})
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await DATABASE.getAllUsers()
+        res.status(200).json({
+            usersArray: users,
+            YOU: res.user.fullName
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: error.message,
+            cause: error.cause
+        })
+    }   
 }
