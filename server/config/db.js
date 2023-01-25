27ce7b4fc28d921f640ef86fada7639394456b10
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-24 23:38:30 -0500

    update db name

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,10 @@
 const mysql2 = require('mysql2')
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const DB = mysql2.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASS,
-  database:process.env.DB_NAME_DEV,
+  database: process.env.DB_NAME,
   port: 3306,
   ssl: {
     rejectUnauthorized: false,
