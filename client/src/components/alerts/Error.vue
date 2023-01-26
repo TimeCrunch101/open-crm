755f57f9bb1dbddc0bf8bb9bb6032657cda0a4fa
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-26 00:10:07 -0500

    created error component

---
+++ client/src/components/alerts/Error.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    errorMessage: String
+})
+
+</script>
+
+<template>
+    <div class="alert alert-danger d-flex align-items-center mt-3" role="alert">
+        <i class="bi bi-exclamation-triangle-fill"></i>
+        <div>
+            {{ errorMessage }}
+        </div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+</template>
+
+
+<style scoped>
+div {
+    width: max-content;
+    margin: auto;
+}
+</style>
