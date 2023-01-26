3c9f1c1af15c805c9c1cef30a7ca9e727573d248
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-26 15:11:20 -0500

    refactoring

--- server/routes/getRouter.js
+++ server/routes/getRouter.js
@@ -4,17 +4,10 @@ const auth = require("../controllers/authController");
 const getRouter = express.Router();
 
 const initGetRouter = (app) => {
-  getRouter.get(
-    "/api/get/users",
-    auth.isAuthenticated,
-    getController.getAllUsers
-  );
+
   getRouter.get("/api/validate", auth.isAuthenticated, auth.validate);
-  getRouter.get(
-    "/api/get/client/notes/:clientID",
-    auth.isAuthenticated,
-    getController.getClientNotes
-  );
+  getRouter.get("/api/get/users", auth.isAuthenticated, getController.getAllUsers);
+  getRouter.get("/api/get/client/notes/:clientID", auth.isAuthenticated, getController.getClientNotes);
 
   return app.use("/", getRouter);
 };
