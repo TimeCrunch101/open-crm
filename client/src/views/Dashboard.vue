eb798b5f16c57ca0a68df0ec3ac1680f79a097ab
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-26 15:12:10 -0500

    muted import

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { useAuthStore } from '../stores/auth';
-const auth = useAuthStore()
+// import { useAuthStore } from '../stores/auth';
+// const auth = useAuthStore()
 
 </script>
 
