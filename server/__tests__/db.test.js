8ac879e57e5110f66f8f445fef97d7be5181139b
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-27 00:00:38 -0500

    created basic unit test

--- server/__tests__/db.test.js
+++ server/__tests__/db.test.js
@@ -1,1 +1,44 @@
+require('dotenv').config()
+const DATABASE = require('../controllers/databaseController')
 
+// TODO: TEST getAllUsers
+
+// TODO: TEST getUserByEmail
+
+test("Register user", async () => {
+    const result = await DATABASE.register('123', 'TEST', "TEST", 'TEST TEST', 'TEST1@TEST1.COM1', 'PASSWORD')
+    expect(result).toEqual(true)
+})
+
+// TODO: TEST disableUser
+
+// TODO: TEST updateUser
+
+test('Delete User', async () => {
+    const res = await DATABASE.deleteUser("123")
+    expect(res).toEqual(true)
+})
+
+// TODO: TEST createClient
+
+// TODO: TEST getClients
+
+// TODO: TEST deleteClient
+
+// TODO: TEST updateClient
+
+// TODO: TEST createClientContact
+
+// TODO: TEST getClientContacts
+
+// TODO: TEST updateClientContact
+
+// TODO: TEST DeleteClientContact
+
+// TODO: TEST createClientNote
+
+// TODO: TEST getClientNotes
+
+// TODO: TEST updateClientNote
+
+// TODO: TEST DeleteClientNote
