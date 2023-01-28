f1109ed699c889d5201ee89dbbdd40d46fa8bd8b
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-27 21:41:18 -0500

    more tests

--- server/__tests__/db.test.js
+++ server/__tests__/db.test.js
@@ -1,14 +1,21 @@
 require('dotenv').config()
 const DATABASE = require('../controllers/databaseController')
 
-// TODO: TEST getAllUsers
-
-// TODO: TEST getUserByEmail
-
 test("Register user", async () => {
     const result = await DATABASE.register('123', 'TEST', "TEST", 'TEST TEST', 'TEST1@TEST1.COM1', 'PASSWORD')
     expect(result).toEqual(true)
 })
+
+test("Get Users", async () => {
+    const res = await DATABASE.getAllUsers()
+    expect(res.length).toBeGreaterThanOrEqual(0)
+})
+
+test('Get User By Email', async () => {
+    const res = await DATABASE.getUserByEmail('TEST1@TEST1.COM1')
+    expect(res.email).toEqual('TEST1@TEST1.COM1')
+})
+
 
 // TODO: TEST disableUser
 
@@ -21,9 +28,33 @@ test('Delete User', async () => {
 
 // TODO: TEST createClient
 
+test('Create Client', async () => {
+    const res = await DATABASE.createClient({
+        companyName: 'Test Company',
+        street: '123 Test Street',
+        city: "TEST CITY",
+        state: 'TEST STATE',
+        zip: '12345',
+        country: "TEST COUNTRY",
+        primaryPhone: "123-456-7899",
+        fax: "987-654-3211",
+        website: "www.test.com"
+    })
+    expect(res).toEqual(true)
+})
+
 // TODO: TEST getClients
 
-// TODO: TEST deleteClient
+test('Get Clients', async () => {
+    const res = await DATABASE.getClients()
+    expect(res.length).toBeGreaterThanOrEqual(0)
+})
+
+test('Delete Client', async () => {
+    const id = await DATABASE.getClient('Test Company')
+    const res = await DATABASE.deleteClient(id.id)
+    expect(res).toEqual(true)
+})
 
 // TODO: TEST updateClient
 
