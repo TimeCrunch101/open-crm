80af16e83a47971fb0577632665235ca88b76f79
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-28 22:55:04 -0500

    Update Success.vue

--- client/src/components/alerts/Success.vue
+++ client/src/components/alerts/Success.vue
@@ -1,16 +1,20 @@
 <script setup>
-const message = defineProps({
-    
+const props = defineProps({
+  message: String
 })
 
 </script>
 
 <template>
-<div class="alert alert-info" role="alert">
+<div class="alert alert-info m-auto" role="alert">
   {{ message }}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 </template>
 
 <style scoped>
+.alert-info {
+  width: fit-content;
+}
 
 </style>
