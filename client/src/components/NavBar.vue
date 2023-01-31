0446afb40089f815519c0b2beff5c053bcbb151a
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-30 23:10:15 -0500

    added profile menu

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,8 +1,18 @@
 <script setup>
 import { useAuthStore } from '../stores/auth';
 import { useRouter } from 'vue-router';
+import { ref } from 'vue'
 const auth = useAuthStore()
 const router = useRouter()
+
+const initials = ref(null)
+
+if (auth.getUser) {
+  const nameArray = auth.getUser.split(" ", [2])
+  const firstInitial = nameArray[0].charAt(0)
+  const lastInitial = nameArray[1].charAt(0)
+  initials.value = firstInitial + lastInitial
+}
 
 const logout = () => {
   auth.logoutUser()
@@ -27,11 +37,9 @@ const logout = () => {
           <li class="nav-item">
             <router-link class="nav-link" aria-current="page" to="/clients">Clients</router-link>
           </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/users">Users</router-link>
-          </li>
           <li class="nav-item dropdown">
-            <router-link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Create</router-link>
+            <router-link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">Create</router-link>
             <ul class="dropdown-menu">
               <li><router-link class="dropdown-item" to="/create/client">New Client</router-link></li>
               <li><router-link class="dropdown-item" to="#">New User</router-link></li>
@@ -49,11 +57,51 @@ const logout = () => {
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
+        <div class="btn-group dropstart">
+          <div class="profile-picture" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="0,0">
+            <span class="profile-initials">{{ initials }}</span>
+          </div>
+          <ul class="dropdown-menu dropdown-menu-start">
+            <li><a class="dropdown-item" href="#">Account Settings</a></li>
+            <li><router-link class="dropdown-item" to="/users">CRM Admin</router-link></li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
 </template>
 
-<style scoped>
 
+
+
+
+<style scoped>
+.profile-picture {
+  margin-left: 1em;
+  outline: 1px solid rgb(0, 0, 0);
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgb(3, 31, 48);
+}
+
+.profile-picture:hover {
+  cursor: pointer;
+}
+
+.profile-initials {
+  font-size: 1.5rem;
+  vertical-align: center;
+  margin-bottom: 5px;
+}
+
+.dropstart .dropdown-menu {
+  top: 57px;
+  right: 0px
+}
 </style>
