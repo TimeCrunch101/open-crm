88e47beea639dbd3b15aeb40e554bea62c3e2b4d
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-02-05 01:50:50 -0500

    chartjs

---
+++ client/src/components/BarChart.vue
@@ -0,0 +1,39 @@
+  
+<script setup>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { ref } from 'vue';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+const chartOptions = ref({
+    responsive: true
+})
+
+const chartData = ref({
+    labels: ['January', 'February', 'March'],
+    datasets: [{
+        label: 'Dataset',
+        backgroundColor: '#177c9e',
+        data: [50,20,12]
+    }]
+})
+
+const myStyles = ref([
+    {"background-color": '#acacac'}
+])
+
+</script>
+
+<template>
+    <div>
+        <Bar :style="myStyles"  id="my-chart-id" :options="chartOptions" :data="chartData"/>
+    </div>
+</template>
+
+<style scoped>
+div {
+    width: 600px;
+    background-color: #acacac;
+}
+</style>
